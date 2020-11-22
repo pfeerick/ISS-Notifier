30892054d6f7f6b0af10ab3f400de2495c357631
Peter Feerick <peter.feerick@gmail.com>
2020-11-22 19:34:27 +1000

    Code re-write to use state machine

--- src/main.cpp
+++ src/main.cpp
@@ -10,64 +10,62 @@
 #include <Adafruit_NeoPixel.h>
 #include "secrets.h"
 
-//Variables for times and duration
-long riseTime = 0;
-long currentTime = 0;
-long duration = 0;
-long timeUntilFlyover = 0; //difference
+#define NEOPIXEL_PIN D3
+#define NUM_OF_NEOPIXELS 17
 
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(12, 2, NEO_GRB + NEO_KHZ800);
+long riseTime = 0;                 // Time the ISS will rise for current position
+long currentTime = 0;              // Current time for GMT
+long flyoverDuration = 0;          // Duration of ISS pass for current position
+long timeUntilFlyover = 0;         // How long it will be until the next flyover
+long timeUntilFlyoverComplete = 0; // How long it will be until the current flyover is complete
+
+enum machineStates
+{
+  START,
+  GET_TIME,
+  GET_NEXT_PASS,
+  WAIT_FOR_PASS,
+  PASS_START,
+  PASSING,
+  PASS_COMPLETE
+};
+
+enum machineStates currentState = START;
+
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUM_OF_NEOPIXELS, NEOPIXEL_PIN, NEO_GRB + NEO_KHZ800);
 WiFiClient client;
 
 void success()
 {
-  for (int i = 0; i < 12; i++)
-  {
-    pixels.setPixelColor(i, pixels.Color(0, 255, 0));
-    pixels.show();
-  }
+  pixels.fill(pixels.Color(0, 255, 0));
+  pixels.show();
 
   delay(500);
 
-  for (int i = 11; i >= 0; i--)
-  {
-    pixels.setPixelColor(i, LOW);
-    pixels.show();
-  }
+  pixels.clear();
+  pixels.show();
 }
 
 void fail()
 {
-  for (int i = 0; i < 12; i++)
-  {
-    pixels.setPixelColor(i, pixels.Color(255, 0, 0));
-    pixels.show();
-  }
+  pixels.fill(pixels.Color(255, 0, 0));
+  pixels.show();
 
   delay(500);
 
-  for (int i = 11; i >= 0; i--)
-  {
-    pixels.setPixelColor(i, LOW);
-    pixels.show();
-  }
+  pixels.clear();
+  pixels.show();
 }
 
 // Current time (GMT)
-void getCurrentTime()
+bool getCurrentTime()
 {
-  Serial.println("Getting the current time...");
-
   HTTPClient http;
   http.begin(client, "http://worldtimeapi.org/api/timezone/europe/london"); //URL for getting the current time
-
   int httpCode = http.GET();
 
   if (httpCode == HTTP_CODE_OK)
   { //Check for the returning code
-
-    success();
-
     String payload = http.getString();
 
     const size_t capacity = JSON_OBJECT_SIZE(15) + 550;
@@ -79,159 +77,235 @@ void getCurrentTime()
     {
       Serial.print(F("deserializeJson() failed(current time): "));
       Serial.println(error.c_str());
-      return;
+      return false;
     }
     currentTime = doc["unixtime"]; //save current time
-    Serial.print("current time= ");
-    Serial.println(currentTime);
+    return true;
   }
   else
   {
-    Serial.println("Error on HTTP request");
-    fail();
+    Serial.println("getCurrentTime(): Error on HTTP request");
+    return false;
   }
 }
 
-void apiCall()
+bool getNextPass()
 {
-  Serial.println("Looking up next ISS flyover time...");
-  if ((WiFi.status() == WL_CONNECTED))
+  HTTPClient http;
+  http.begin(client, "http://api.open-notify.org/iss-pass.json?lat=" + String(latitude) + "&lon=" + String(longitude) + "&alt=" + String(altitude) + "&n=5"); //URL for API call
+
+  int httpCode = http.GET();
+
+  if (httpCode == HTTP_CODE_OK)
   {
-    getCurrentTime(); //call function for getting the current time
+    String payload = http.getString(); //save response
 
-    HTTPClient http;
-    http.begin(client, "http://api.open-notify.org/iss-pass.json?lat=" + String(latitude) + "&lon=" + String(longitude) + "&alt=" + String(altitude) + "&n=5"); //URL for API call
+    const size_t capacity = JSON_ARRAY_SIZE(5) + 5 * JSON_OBJECT_SIZE(2) + JSON_OBJECT_SIZE(3) + JSON_OBJECT_SIZE(5) + 190;
+    DynamicJsonDocument doc(capacity);
+    DeserializationError error = deserializeJson(doc, payload);
 
-    int httpCode = http.GET();
+    http.end();
 
-    if (httpCode == HTTP_CODE_OK)
+    if (error)
     {
-      success();
-
-      String payload = http.getString(); //save response
-
-      const size_t capacity = JSON_ARRAY_SIZE(5) + 5 * JSON_OBJECT_SIZE(2) + JSON_OBJECT_SIZE(3) + JSON_OBJECT_SIZE(5) + 190;
-      DynamicJsonDocument doc(capacity);
-      DeserializationError error = deserializeJson(doc, payload);
-
-      http.end();
-
-      if (error)
-      {
-        Serial.print(F("deserializeJson() failed: "));
-        Serial.println(error.c_str());
-        return;
-      }
-
-      JsonArray response = doc["response"];
-      duration = response[0]["duration"]; // save duration of the next flyover
-      riseTime = response[0]["risetime"]; // save start time of the next flyover
-
-      if (riseTime < currentTime)
-      { //If ISS has already passed, take the next flyover
-        duration = response[1]["duration"];
-        riseTime = response[1]["risetime"];
-      }
-
-      Serial.print("Risetime [0]= ");
-      Serial.println(riseTime);
-
-      //compute time until rise
-      timeUntilFlyover = riseTime - currentTime;
-      Serial.print("Time until flyover (in seconds): ");
-      Serial.println(timeUntilFlyover);
-
-      uint32_t t = timeUntilFlyover;
-      int s = t % 60;
-      t = (t - s)/60;
-      int m = t % 60;
-      t = (t - m)/60;
-      int h = t;
-      Serial.printf("That's %02i hours, %02i minutes and %02i seconds\n",h,m,s);
+      Serial.print(F("deserializeJson() failed: "));
+      Serial.println(error.c_str());
+      return false;
     }
-    else
-    {
-      Serial.println("Error on HTTP request");
-      fail();
+
+    JsonArray response = doc["response"];
+    flyoverDuration = response[0]["duration"]; // save duration of the next flyover
+    riseTime = response[0]["risetime"];        // save start time of the next flyover
+
+    if (riseTime < currentTime)
+    { //If ISS has already passed, take the next flyover
+      flyoverDuration = response[1]["duration"];
+      riseTime = response[1]["risetime"];
     }
+
+    //compute time until rise
+    timeUntilFlyover = riseTime - currentTime;
+    return true;
+  }
+  else
+  {
+    Serial.println("getNextPass(): Error on HTTP request");
+    return false;
   }
 }
 
 void setup()
 {
   Serial.begin(115200);
-  pinMode(LED_BUILTIN, OUTPUT); //LED Pin
-  digitalWrite(LED_BUILTIN, LOW); // invert if LOW == ON
+  pinMode(LED_BUILTIN, OUTPUT);    //LED Pin
+  digitalWrite(LED_BUILTIN, HIGH); // invert if HIGH == ON
 
   pixels.begin();
   pixels.setBrightness(100);
+  pixels.show();
 
   Serial.println("");
-  Serial.println("ISS Notifier");
+  Serial.println("ISS Flyover Notifier");
   Serial.println("");
 
   WiFi.begin(ssid, password);
   Serial.print("WiFi Connecting...");
-  while (WiFi.status() != WL_CONNECTED)
+  while ((WiFi.status() != WL_CONNECTED) && (millis() < 30000))
   {
     delay(100);
     Serial.print(".");
-    digitalWrite(LED_BUILTIN,!digitalRead(LED_BUILTIN));
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
   }
 
-  if (WiFi.status() == WL_CONNECTED) {
+  digitalWrite(LED_BUILTIN, HIGH); // invert if HIGH == ON
+
+  if (WiFi.status() == WL_CONNECTED)
+  {
     success();
-    Serial.println("[CONNECTED!]");
-  } else{
-    fail();
-    Serial.println("[FAIL!]");
+    Serial.println("CONNECTED!");
+  }
+  else
+  {
+    Serial.println("FAIL!");
+    Serial.print("Device will restart in ");
+    for (int i = 5; i >= 1; i--)
+    {
+      Serial.print(i);
+      Serial.print("...");
+      fail();
+      delay(500);
+    }
+    ESP.restart();
   }
 }
 
 void loop()
 {
-  apiCall(); //API call for the next ISS flyover
-
-  //shut down the NeoPixel until next ISS flyover
-  for (int i = 0; i < 12; i++)
+  switch (currentState)
   {
-    pixels.setPixelColor(i, LOW);
+  case START:
+  {
+    //shut down the NeoPixel until next ISS flyover
+    pixels.clear();
     pixels.show();
-  }
 
-  while (timeUntilFlyover > 0)
-  { // while the ISS isn't overhead
-
-    delay(1000);
-    Serial.println(timeUntilFlyover);
-
-    timeUntilFlyover--;
-  }
-
-  //when ISS rises  above the horizon
-  Serial.println("ISS overhead!");
-  int maxDuration = duration; //save max value of the flyover duration
-  Serial.print("max duration = ");
-  Serial.println(duration);
-
-  for (duration; duration >= 0; duration--)
-  {
-    //map remaining flyover time on a color gradient
-    int colorRed = map(duration, 0, maxDuration, 200, 0);
-    int colorBlue = map(duration, 0, maxDuration, 0, 200);
-
-    //show the current color on all LEDs
-    for (int i = 0; i < 12; i++)
+    if (WiFi.status() == WL_CONNECTED)
     {
-      pixels.setPixelColor(i, pixels.Color(colorRed, 0, colorBlue));
+      currentState = GET_TIME;
+    }
+    break;
+  }
+  case GET_TIME:
+  {
+    Serial.println("Getting the current time (GMT)...");
+    if (!getCurrentTime())
+    {
+      fail();
+      delay(1000);
+    }
+    else
+    {
+      Serial.print("Current time (GMT) = ");
+      Serial.println(currentTime);
+      success();
+      currentState = GET_NEXT_PASS;
+    }
+    break;
+  }
+  case GET_NEXT_PASS:
+  {
+    Serial.println("Looking up next ISS flyover time...");
+    if (!getNextPass())
+    {
+      fail();
+      delay(1000);
+    }
+    else
+    {
+      success();
+
+      Serial.print("Risetime = ");
+      Serial.println(riseTime);
+
+      Serial.print("Time until flyover (in seconds): ");
+      Serial.println(timeUntilFlyover);
+
+      uint32_t t = timeUntilFlyover;
+      int s = t % 60;
+      t = (t - s) / 60;
+      int m = t % 60;
+      t = (t - m) / 60;
+      int h = t;
+      Serial.printf("That's %02i hours, %02i minutes and %02i seconds from now\n", h, m, s);
+
+      timeUntilFlyoverComplete = flyoverDuration;
+      currentState = WAIT_FOR_PASS;
+    }
+    break;
+  }
+  case WAIT_FOR_PASS:
+  {
+    if (timeUntilFlyover > 0)
+    { // while the ISS isn't overhead
+
+      uint32_t t = timeUntilFlyover;
+      int s = t % 60;
+      t = (t - s) / 60;
+      int m = t % 60;
+      t = (t - m) / 60;
+      int h = t;
+      Serial.printf("Next ISS flyover in %02i:%02i:%02i\n", h, m, s);
+
+      timeUntilFlyover--;
+      delay(1000);
+    }
+    else
+    {
+      currentState = PASS_START;
+    }
+    break;
+  }
+  case PASS_START:
+  {
+    //when ISS rises  above the horizon
+    Serial.println("ISS overhead!");
+    Serial.print("max duration = ");
+    Serial.println(timeUntilFlyoverComplete);
+    currentState = PASSING;
+    break;
+  }
+  case PASSING:
+  {
+    if (timeUntilFlyoverComplete > 0)
+    {
+      //map remaining flyover time on a color gradient
+      int colorRed = map(timeUntilFlyoverComplete, 0, flyoverDuration, 200, 0);
+      int colorBlue = map(timeUntilFlyoverComplete, 0, flyoverDuration, 0, 200);
+
+      //show the current color on all LEDs
+      for (int i = 0; i < NUM_OF_NEOPIXELS; i++)
+      {
+        pixels.setPixelColor(i, pixels.Color(colorRed, 0, colorBlue));
+        pixels.show();
+      }
+      timeUntilFlyoverComplete--;
+      delay(1000);
+    }
+    else
+    {
+      currentState = PASS_COMPLETE;
+    }
+    break;
+  }
+  case PASS_COMPLETE:
+  {
+    for (int i = 0; i < NUM_OF_NEOPIXELS; i++)
+    {
+      pixels.setPixelColor(i, LOW);
       pixels.show();
     }
-    delay(1000);
+    currentState = START;
+    break;
   }
-
-  for (int i = 0; i < 12; i++)
-  {
-    pixels.setPixelColor(i, LOW);
-    pixels.show();
   }
 }
